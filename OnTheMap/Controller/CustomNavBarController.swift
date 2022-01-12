f746cf3f7607efb7f6dbbda5ad85e3f2f785cc93
Dhara Bhavsar <dhara.bhavsar@salesforce.com>
2022-01-11 23:26:29 -0500

    Added custom nav controller such that nav controller is similar for map and table views

---
+++ OnTheMap/Controller/CustomNavBarController.swift
@@ -0,0 +1,19 @@
+//
+//  CustomNavBarController.swift
+//  OnTheMap
+//
+//  Created by Dhara Bhavsar on 2022-01-11.
+//  Copyright © 2022 Dhara Bhavsar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomNavBarController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
